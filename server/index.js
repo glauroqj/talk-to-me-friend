dfbdb01f8d97b25ba1cdb8aef85be364445a705e
glauroqj <glauro.juliani@hotmail.com>
2020-05-03 13:22:36 -0300

    add: nodemon

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express()
 const environment = process.env.NODE_ENV || 'development'
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello world!')
+  res.status(200).send('Hello world!!')
 })
- 
+
 app.listen(3000)
