02942330c084df62a8a7c8c557d7555d96035a70
glauroqj <glauro.juliani@hotmail.com>
2020-05-03 13:17:39 -0300

    start server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const app = express()
+const environment = process.env.NODE_ENV || 'development'
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello world!')
+})
+ 
+app.listen(3000)
