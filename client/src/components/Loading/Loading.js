076722dbc665738b35372fea020ace3bd73d60d2
glauroqj <glauro.juliani@hotmail.com>
2020-05-07 12:35:48 -0300

    fix: layout

---
+++ client/src/components/Loading/Loading.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+/** style */
+import * as El from './Loading.style'
+/** components */
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+const Loading = ({text, color}) => (
+  <El.LoadingContainer>
+    <CircularProgress color={color} />
+    {text}
+  </El.LoadingContainer>
+)
+
+Loading.defaultProps = {
+  text: '',
+  color: 'primary'
+}
+
+Loading.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string
+}
+
+export default Loading
