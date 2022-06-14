e29a4b314d12f41ba2381716f0371f0c502a3c2b
glauroqj <glauro.juliani@hotmail.com>
2022-06-14 18:38:17 -0300

    add: configure compose and dockerfile

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
-import express from 'express'
-import { Server } from 'http'
-import path from 'path'
-const cookieParser = require('cookie-parser')
-const app = express()
-const server = Server(app)
-const environment = process.env.NODE_ENV || 'development'
+import express from "express";
+import { Server } from "http";
+import path from "path";
+const cookieParser = require("cookie-parser");
+const app = express();
+const server = Server(app);
+const environment = process.env.NODE_ENV || "development";
 /** routes */
-import createRoutes from './routes/createRoutes'
-import createSocket from './middlewares/socket'
+import createRoutes from "./routes/createRoutes";
+import createSocket from "./middlewares/socket";
 /** middlewares */
-app.use( express.static(path.join(__dirname, '../client/build')) )
+app.use(express.static(path.join(__dirname, "../client/build")));
 /** global variables */
-global.rooms = {}
+global.rooms = {};
 
-createRoutes(app)
-createSocket(server)
+createRoutes(app);
+createSocket(server);
 
-server.listen(process.env.PORT || 9000, '0.0.0.0', () => console.log('< SERVER IS RUNNING >'))
+server.listen(process.env.PORT, "0.0.0.0", () =>
+  console.log("< SERVER IS RUNNING >")
+);
