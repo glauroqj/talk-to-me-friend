3f10dfabde9dc703067587aa2ab41b9ceb7e7f44
glauroqj <glauro.juliani@hotmail.com>
2022-06-19 19:34:12 -0300

    add

--- client/src/components/Channel/Channel.js
+++ client/src/components/Channel/Channel.js
@@ -21,10 +21,6 @@ const Channel = ({ socket }) => {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    /** ONLY SOCKET CONNECTIONs */
-  }, []);
-
-  useEffect(() => {
     // if (socket?.connected) {
     //   console.log("< SOCKET CONNECTED > ", socket);
     //   handleConnection();
@@ -34,23 +30,17 @@ const Channel = ({ socket }) => {
     //   setError(true);
     //   setIsLoading(false);
     // }
-    // getClientConnection();
+    getClientConnection();
   }, [socket]);
 
   const getClientConnection = () => {
     if (!socket?.connected) return false;
-    // var peer = new Peer(undefined, {
-    //   path: "/",
-    //   host: "localhost",
-    //   port: 9000,
-    // });
-
     console.log("< CHANNEL - SOCKET > ", socket);
     const { id } = socket;
-
     let checkElement = document.getElementById(`attendant-${id}`);
-
-    // Free public STUN servers provided by Google.
+    let myVideoStream;
+    let myVideo = document.createElement("video");
+    let rtcPeerConnection;
     const iceServers = {
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
@@ -61,9 +51,34 @@ const Channel = ({ socket }) => {
       ],
     };
 
-    let rtcPeerConnection;
-    let myVideoStream;
-    let myVideo = document.createElement("video");
+    /** ONLY SOCKET CONNECTIONs */
+    socket.on("webrtc_offer", async (event) => {
+      console.log("Socket event callback: webrtc_offer");
+
+      rtcPeerConnection = new RTCPeerConnection(iceServers);
+      _helperAddLocalTracks(rtcPeerConnection, myVideoStream);
+      rtcPeerConnection.ontrack = _helperSetRemoteStream;
+      rtcPeerConnection.onicecandidate = _helperSendIceCandidate;
+      rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+      // await _helperHandleOfferAnswer(rtcPeerConnection);
+    });
+
+    socket.on("webrtc_answer", (event) => {
+      console.log("Socket event callback: webrtc_answer ", event);
+
+      rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+    });
+
+    socket.on("webrtc_ice_candidate", (event) => {
+      console.log("Socket event callback: webrtc_ice_candidate ", event);
+
+      // ICE candidate configuration.
+      let candidate = new RTCIceCandidate({
+        sdpMLineIndex: event.label,
+        candidate: event.candidate,
+      });
+      rtcPeerConnection.addIceCandidate(candidate);
+    });
 
     if (checkElement) {
       /** duplicated element */
@@ -84,7 +99,6 @@ const Channel = ({ socket }) => {
     const _addVideoStream = (video, stream) => {
       video.srcObject = stream;
       video.addEventListener("loadedmetadata", async () => {
-        /** connect - start */
         rtcPeerConnection = new RTCPeerConnection(iceServers);
         _helperAddLocalTracks(rtcPeerConnection, myVideoStream);
         rtcPeerConnection.ontrack = _helperSetRemoteStream;
@@ -115,6 +129,13 @@ const Channel = ({ socket }) => {
 
   const _helperSendIceCandidate = (event) => {
     console.log("< _helperSendIceCandidate > ", event);
+    if (event.candidate) {
+      socket.emit("webrtc_ice_candidate", {
+        roomId: String(window?.location?.pathname),
+        label: event?.candidate?.sdpMLineIndex,
+        candidate: event?.candidate?.candidate,
+      });
+    }
   };
 
   const _helperHandleOfferAnswer = (rtcPeerConnection) => {
@@ -166,115 +187,115 @@ const Channel = ({ socket }) => {
   };
   /** END HELPER FUNCTIONS */
 
-  const handleConnection = async () => {
-    if (!isRTCLoaded || !isIOLoaded) return false;
-    console.log("< CHECK LIB > ", isRTCLoaded, isIOLoaded);
+  // const handleConnection = async () => {
+  //   if (!isRTCLoaded || !isIOLoaded) return false;
+  //   console.log("< CHECK LIB > ", isRTCLoaded, isIOLoaded);
 
-    const RTCMultiConnection = window.RTCMultiConnection;
-    const connection = new RTCMultiConnection();
-    connection.socketURL = "https://rtcmulticonnection.herokuapp.com:443/";
-    connection.session = {
-      audio: true,
-      video: true,
-    };
-    connection.sdpConstraints.mandatory = {
-      OfferToReceiveAudio: true,
-      OfferToReceiveVideo: true,
-    };
-    connection.mediaConstraints = {
-      video: {
-        width: { ideal: 1280 },
-        height: { ideal: 720 },
-        frameRate: 30,
-      },
-      audio: true,
-    };
-    connection.iceServers = [
-      {
-        urls: [
-          "stun:stun.l.google.com:19302",
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-          "stun:stun.l.google.com:19302?transport=udp",
-        ],
-      },
-    ];
+  //   const RTCMultiConnection = window.RTCMultiConnection;
+  //   const connection = new RTCMultiConnection();
+  //   connection.socketURL = "https://rtcmulticonnection.herokuapp.com:443/";
+  //   connection.session = {
+  //     audio: true,
+  //     video: true,
+  //   };
+  //   connection.sdpConstraints.mandatory = {
+  //     OfferToReceiveAudio: true,
+  //     OfferToReceiveVideo: true,
+  //   };
+  //   connection.mediaConstraints = {
+  //     video: {
+  //       width: { ideal: 1280 },
+  //       height: { ideal: 720 },
+  //       frameRate: 30,
+  //     },
+  //     audio: true,
+  //   };
+  //   connection.iceServers = [
+  //     {
+  //       urls: [
+  //         "stun:stun.l.google.com:19302",
+  //         "stun:stun1.l.google.com:19302",
+  //         "stun:stun2.l.google.com:19302",
+  //         "stun:stun.l.google.com:19302?transport=udp",
+  //       ],
+  //     },
+  //   ];
 
-    connection.onstream = (event) => {
-      console.log("< ON STREAM > ", event);
+  //   connection.onstream = (event) => {
+  //     console.log("< ON STREAM > ", event);
 
-      let checkElement = document.getElementById(`attendant-${event.streamid}`);
+  //     let checkElement = document.getElementById(`attendant-${event.streamid}`);
 
-      if (checkElement) {
-        /** duplicated element */
-        checkElement.remove();
-      }
+  //     if (checkElement) {
+  //       /** duplicated element */
+  //       checkElement.remove();
+  //     }
 
-      event.mediaElement.removeAttribute("src");
-      event.mediaElement.removeAttribute("srcObject");
-      event.mediaElement.muted = true;
-      event.mediaElement.volume = 0;
+  //     event.mediaElement.removeAttribute("src");
+  //     event.mediaElement.removeAttribute("srcObject");
+  //     event.mediaElement.muted = true;
+  //     event.mediaElement.volume = 0;
 
-      let video = document.createElement("video");
-      video.setAttribute("id", `attendant-${event.streamid}`);
-      video.setAttribute("class", "animated fadeIn");
+  //     let video = document.createElement("video");
+  //     video.setAttribute("id", `attendant-${event.streamid}`);
+  //     video.setAttribute("class", "animated fadeIn");
 
-      try {
-        video.setAttributeNode(document.createAttribute("autoplay"));
-        video.setAttributeNode(document.createAttribute("playsinline"));
-      } catch (e) {
-        video.setAttribute("autoplay", true);
-        video.setAttribute("playsinline", true);
-      }
+  //     try {
+  //       video.setAttributeNode(document.createAttribute("autoplay"));
+  //       video.setAttributeNode(document.createAttribute("playsinline"));
+  //     } catch (e) {
+  //       video.setAttribute("autoplay", true);
+  //       video.setAttribute("playsinline", true);
+  //     }
 
-      if (event.type === "local") {
-        window.userIdLocal = event.streamid;
-        video.volume = 0;
+  //     if (event.type === "local") {
+  //       window.userIdLocal = event.streamid;
+  //       video.volume = 0;
 
-        try {
-          video.setAttributeNode(document.createAttribute("muted"));
-        } catch (e) {
-          video.setAttribute("muted", true);
-        }
-      }
+  //       try {
+  //         video.setAttributeNode(document.createAttribute("muted"));
+  //       } catch (e) {
+  //         video.setAttribute("muted", true);
+  //       }
+  //     }
 
-      document.getElementById("attendants").appendChild(video);
+  //     document.getElementById("attendants").appendChild(video);
 
-      /** update users */
-      const userArrays = connection.streamEvents.selectAll();
-      setUsers(userArrays);
+  //     /** update users */
+  //     const userArrays = connection.streamEvents.selectAll();
+  //     setUsers(userArrays);
 
-      setTimeout(() => {
-        setIsLoading(false);
-        video.srcObject = event.stream;
-      }, 300);
-    };
+  //     setTimeout(() => {
+  //       setIsLoading(false);
+  //       video.srcObject = event.stream;
+  //     }, 300);
+  //   };
 
-    connection.onstreamended = (event) => {
-      let checkElement = document.getElementById(`attendant-${event.streamid}`);
-      if (checkElement) {
-        checkElement.remove();
+  //   connection.onstreamended = (event) => {
+  //     let checkElement = document.getElementById(`attendant-${event.streamid}`);
+  //     if (checkElement) {
+  //       checkElement.remove();
 
-        /** update users */
-        setTimeout(() => {
-          toast.warn(`${event.streamid} saiu`);
-          const userArrays = connection.streamEvents.selectAll();
-          setUsers(userArrays);
-        }, 500);
-      }
-    };
+  //       /** update users */
+  //       setTimeout(() => {
+  //         toast.warn(`${event.streamid} saiu`);
+  //         const userArrays = connection.streamEvents.selectAll();
+  //         setUsers(userArrays);
+  //       }, 500);
+  //     }
+  //   };
 
-    connection.onMediaError = (e) => {
-      console.warn("< ERROR > ", e);
-      if (e.message === "Concurrent mic process limit.") {
-        connection.join(connection.sessionid);
-      }
-    };
+  //   connection.onMediaError = (e) => {
+  //     console.warn("< ERROR > ", e);
+  //     if (e.message === "Concurrent mic process limit.") {
+  //       connection.join(connection.sessionid);
+  //     }
+  //   };
 
-    connection.openOrJoin(window.location.pathname);
-    console.log("< connection > ", connection);
-    window.connection = connection;
-  };
+  //   connection.openOrJoin(window.location.pathname);
+  //   console.log("< connection > ", connection);
+  //   window.connection = connection;
+  // };
 
   return (
     <El.ChannelContainer>
